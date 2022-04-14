e66495147967c09060a0f874e30c49eb316f2312
vinhtrieulnvt <vinhtrieulnvt@gmail.com>
2022-04-15 00:28:57 +0700

    change name

--- bai6.js
+++ bai6.js
@@ -1,11 +1,11 @@
 const numbers = [1, 2, 3, 4];
 
-function doubleEvenNumber(numbers) {
+function doubleOddNumberAndDivEvenNumber(numbers) {
     return numbers.map((number) => number % 2 == 0 ? number / 2 : number * 2);
 }
 
 function sum(numbers) {
-    const a = doubleEvenNumber(numbers);
+    const a = doubleOddNumberAndDivEvenNumber(numbers);
     return a.reduce((s, e) => s + e);
 }
 
